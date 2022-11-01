c8cd96ce92e81989f7f57bc875200e3089c176e4
Konrad Szatan <konrad.szatan@tink.com>
2022-11-01 16:04:40 +0100

    DEMO

---
+++ list_topics.sh
@@ -0,0 +1,1 @@
+./kafka-tools/bin/kafka-topics.sh --bootstrap-server localhost:9094 --list
---
+++ read_records.sh
@@ -0,0 +1,1 @@
+./kafka-tools/bin/kafka-console-consumer.sh --bootstrap-server localhost:9094 --topic topicOne --from-beginning
